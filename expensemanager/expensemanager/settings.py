675e1ddfea05b85373b844ed98416e4083dd2ee2
Ankitkumarbishwas <ankitkuamrbishwas2003@gmail.com>
2026-08-23 23:17:47 +0530

    update

--- expensemanager/expensemanager/settings.py
+++ expensemanager/expensemanager/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-93r4@9uj2$^g=inq^b=5$(w-%90401l0_z5k%@t9i+od4+djbx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.whiteNoiseMiddleware',
+    #'whitenoise.middleware.whiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
